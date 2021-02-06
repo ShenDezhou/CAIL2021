378a1d0cf281b7ab37cf6affc77a70bca2556978
ShenDezhou <tsinghua9boy@sina.com>
2021-02-06 20:04:42 +0800

    1. Wrote the Word Piece tokenizer codes.

---
+++ coffeeformer/wpe_tokenizer_scratch.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from tokenizers import BertWordPieceTokenizer
+
+
+paths = [str(x) for x in Path("./data/").glob("**/data*train.csv")]
+
+# Initialize a tokenizer
+tokenizer = BertWordPieceTokenizer()
+
+# Customize training
+tokenizer.train(files=paths, vocab_size=21_128, min_frequency=0, special_tokens=[
+                "[PAD]",
+                "[UNK]",
+                "[CLS]",
+                "[SEP]",
+                "[MASK]",
+                "<S>",
+                "<T>"
+                ])
+
+
+# Save files to disk
+tokenizer.save_model("model/wpe", "")
