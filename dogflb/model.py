8b31441427b0b1d9d7f94afee13cb2e7ac2d5c11
ShenDezhou <bangtech@sina.com>
2021-02-07 16:26:15 +0800

    1. modify the model, limit the output to 130 classes.

--- dogflb/model.py
+++ dogflb/model.py
@@ -3,8 +3,10 @@ import jittor.nn as nn
 
 class Net(nn.Module):
     def __init__(self, num_classes):
-        self.base_net = Resnet50(num_classes)
+        self.base_net = Resnet50()
+        self.fc = nn.Linear(1000, num_classes)
     
     def execute(self, x):
         x = self.base_net(x)
+        x = self.fc(x)
         return x 
