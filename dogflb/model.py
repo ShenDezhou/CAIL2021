7e30ec5a20c79ed20a7d93d96a7f4eab9f2d0333
ShenDezhou <bangtech@sina.com>
2021-03-13 21:26:15 +0800

    1.gelu activation replacement for resnet.

--- dogflb/model.py
+++ dogflb/model.py
@@ -4,6 +4,7 @@ from seresnet import resnet152 as seresnet152
 from cbamresnet import resnet152 as caresnet152
 from searesnet import resnet152 as searesnet152
 from aresnet import resnet152 as aresnet152
+from gresnet import resnet152 as gresnet152
 import jittor.nn as nn
 
 class Net1(nn.Module):
@@ -170,9 +171,20 @@ class Net13(nn.Module):
         x = self.fc(x)
         return x
 
-class Net(nn.Module):
+class Net14(nn.Module):
     def __init__(self, num_classes):
         self.base_net = aresnet152(pretrained=True)
+        self.fc = nn.Linear(1000, num_classes)
+
+    def execute(self, x):
+        x = self.base_net(x)
+        x = self.fc(x)
+        return x
+
+
+class Net(nn.Module):
+    def __init__(self, num_classes):
+        self.base_net = gresnet152(pretrained=True)
         self.fc = nn.Linear(1000, num_classes)
 
     def execute(self, x):
